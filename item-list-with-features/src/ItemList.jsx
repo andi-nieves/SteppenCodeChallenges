59fdacc48e7503b5008b9ea6513e8d7694f2670d
andi-nieves <andinieves151720@gmail.com>
2022-01-14 19:38:14 +0800

    ItemList list handler with actions

--- item-list-with-features/src/ItemList.jsx
+++ item-list-with-features/src/ItemList.jsx
@@ -1,3 +1,18 @@
-export const ItemList = ({ data }) => {
-  return null
+const Button = ({ onClick, icon }) => <a href="/#" onClick={e => {
+  e.preventDefault()
+  onClick()
+}} className="mr-1">
+  <i className={icon}></i>
+</a>
+
+export const ItemList = ({ data, editing, onEdit = () => { }, onDelete = () => { } }) => {
+  return <ul className={editing ? 'editing' : ''}>
+    {data.map((value, index) => <li key={index} className="is-flex">
+      <div className="actions">
+        <Button onClick={onEdit.bind(this, { index, value })} icon="fa fa-pencil has-text-info" />
+        <Button onClick={onDelete.bind(this, { index, value })} icon="fa fa-remove has-text-danger" />
+      </div>
+      <p>{value} {editing?.index === index ? ' (editing)' : ''}</p>
+    </li>)}
+  </ul>
 }
